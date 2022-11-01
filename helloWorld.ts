e8d5e4ecfbf4c1d59d6155400337cb167a8f682f
Quinn Dumala <quinndumala@gmail.com>
2022-11-01 14:53:55 +0800

    update welcome comment position

--- helloWorld.ts
+++ helloWorld.ts
@@ -1,9 +1,9 @@
-
+/*
+If you've opened this code via CodeLink - congrats! 
+You can now create and share your own links too. 
+Select code in this file, then click the lightbulb and 
+"CodeLink: get link" to make your own!
+*/
 function HelloWorld(){
-    /*
-    If you've opened this code via CodeLink - congrats! 
-    You can now create and share your own links too. 
-    Select code in this file, then click the lightbulb and 
-    "CodeLink: get link" to make your own!
-    */
+    console.log("Hello World!");
 }
