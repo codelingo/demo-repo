dc70e4c3ef3a4083953581f3ddc62aa73f83eb58
Quinn Dumala <quinndumala@gmail.com>
2022-11-07 14:51:15 +0800

    Update helloWorld.ts
    format
    
    Co-authored-by: Lachlan Scown <5018471+lscown@users.noreply.github.com>

--- helloWorld.ts
+++ helloWorld.ts
@@ -4,6 +4,6 @@ You can now create and share your own links too.
 Select code in this file, then click the lightbulb and 
 "CodeLink: get link" to make your own!
 */
-function HelloWorld(){
+function HelloWorld() {
     console.log("Hello World!");
 }
